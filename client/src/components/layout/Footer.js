08c223fbb00b4fadbd7868191251673ae324a9a7
Mehdi mezrigui <mehdi.mezrigui@gmail.com>
2019-07-03 14:00:45 +0100

    first commit

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="container-fluid bg-dark text-white mt-5 p-4 text-center ">
+      Copyright &copy; {new Date().getFullYear()} L'Affar
+    </footer>
+  );
+};
